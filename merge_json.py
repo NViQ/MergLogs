c1e3456b99a3a8c1a6fa1cee036c42bb99f24360
Pavel Guliukin <109614855+NViQ@users.noreply.github.com>
2023-03-15 23:21:31 +0500

    Update merge_json.py

--- merge_json.py
+++ merge_json.py
@@ -2,30 +2,32 @@ import json
 import sys
 
 
-def merge_jsonFiles(filename):
-    result = list()
-    for f1 in filename:
-        with open(f1, 'r') as infile:
-            while infile:
-                line = infile.readline()
-                if not line:
-                    break
-                result.append(json.loads(line))
-    sort_result = sorted(result, key=(lambda x: x['timestamp']))
-
-
-    with open(sys.argv[4], 'w') as output_file:
-        json.dump(sort_result, output_file)
-
+def merge_jsonFiles(filename1, filename2, filename3):
+    objects = [filename1, filename2]
+    for file in objects:
+        with open(file, 'r') as infile:
+            for f1 in infile:
+                with open(filename3, 'a+') as output_file:
+                    i = 0
+                    while output_file:
+                        output_file.seek(i)
+                        line = output_file.readline()
+                        if json.loads(line).get('timestamp') >= json.loads(f1).get('timestamp'):
+                            output_file.write(str(json.loads(f1)).replace('\'', '\"'))
+                            output_file.write('\n')
+                            break
+                        if not line:
+                            output_file.write(str(json.loads(f1)))
+                            output_file.write('\n')
+                            break
+                        i += output_file.tell()
 
 def main():
     if len(sys.argv) < 5:
         return
     if sys.argv[3] != '-o':
         return
-    files = [sys.argv[1], sys.argv[2]]
-    merge_jsonFiles(files)
-
+    merge_jsonFiles(sys.argv[1], sys.argv[2], sys.argv[4])
 
 
 if __name__ == '__main__':
