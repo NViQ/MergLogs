438e63d10f2099a64eafe4968684df5ec2240b5a
Pavel Guliukin <109614855+NViQ@users.noreply.github.com>
2023-03-16 01:37:05 +0500

    Create merge_json_version_2.1.py

---
+++ merge_json_version_2.1.py
@@ -0,0 +1,47 @@
+import json
+import argparse
+from pathlib import Path
+
+
+def merge_json(filename1, filename2, filename3):
+    with open(filename1, 'r') as file1, \
+            open(filename2, 'r') as file2, \
+            open(filename3, 'w+') as output_file:
+        try:
+            line1 = file1.readline()
+            line2 = file2.readline()
+            while True:
+                if line1 and line2:
+                    if json.loads(line1).get('timestamp') <= json.loads(
+                            line2).get('timestamp'):
+                        output_file.write(line1)
+                        line1 = file1.readline()
+                    else:
+                        output_file.write(line2)
+                        line2 = file2.readline()
+                elif line1 and not line2:
+                    output_file.write(line1)
+                    line_1 = file1.readline()
+                elif not line1 and line2:
+                    output_file.write(line2)
+                    line2 = file2.readline()
+                elif not line1 and not line2:
+                    break
+        finally:
+            file1.close()
+            file2.close()
+            output_file.close()
+
+
+
+
+def main():
+    if len(sys.argv) < 5:
+        return
+    if sys.argv[3] != '-o':
+        return
+    merge_jsonFiles(sys.argv[1], sys.argv[2], sys.argv[4])
+
+
+if __name__ == '__main__':
+    main()
